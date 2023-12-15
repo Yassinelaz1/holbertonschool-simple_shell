7f54d6d1ef9932f89de10b762300599bb32818d4
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-12-15 11:31:39 +0100

    test file

---
+++ prompt.c
@@ -0,0 +1,82 @@
+#include "shell.h"
+int main(void) 
+{
+	char *command = NULL; //command = null
+	while (1)//main loop 
+	{
+		display_prompt();
+		user_input(&command);
+		execute_command(command);
+		free(command);
+	}
+	return 0;
+}
+
+void display_prompt(void)
+{
+	printf("$ ");
+}
+
+void user_input(char **command)
+{
+	size_t size = 0;
+
+	if (getline(command, &size, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			printf("\n");
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("getline");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	(*command)[strcspn(*command, "\n")] = '\0'; // Remove newline
+}
+
+void execute_command(char *command)
+{
+	pid_t child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (child_pid == 0)
+	{
+		// filston process
+		char *token = strtok(command, " ");
+		char **args = (char **)malloc(2 * sizeof(char *));
+		int i = 0;
+
+		while (token != NULL)
+		{
+			args[i++] = strdup(token);
+			token = strtok(NULL, " ");
+		}
+
+		args[i] = NULL; // Null-terminate the argument list
+
+		execve(args[0], args, NULL);
+		perror("$");
+
+		// Free allocated memory in the filston process
+		for (int j = 0; args[j] != NULL; ++j)
+		{
+			free(args[j]);
+		}
+		free(args);
+
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		// Papa process
+		int status;
+		waitpid(child_pid, &status, 0);
+	}
+}
