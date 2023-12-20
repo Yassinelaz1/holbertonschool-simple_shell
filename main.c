a2d10c2aa079e9c383a1c3a46892df648d8ede82
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-12-20 17:28:11 +0100

    main

--- main.c
+++ main.c
@@ -4,22 +4,23 @@ int main(int argc, char **argv, char **env)
     char *line = NULL;
     char **command = NULL;
     int stat;
-        int i;
+     
     (void)argc;
 
 
     while (1)
     {
         line = display_prompt();
+        if (line == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			return (stat);
+		}
         command = split_line(line);
 
         stat = execute_command(command, argv, env);
-        for ( i = 0; command[i]; i++)
-        {
-            free(command[i]);
-            command[i] = NULL;
-        }
-        free(command);
+        
     }
     return (stat);
 }
