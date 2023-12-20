a6096e74db84a4228b13df74bb86a38603041b22
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-12-20 16:53:08 +0100

    main loop

--- main.c
+++ main.c
@@ -4,20 +4,22 @@ int main(int argc, char **argv, char **env)
     char *line = NULL;
     char **command = NULL;
     int stat;
+        int i;
     (void)argc;
-    
+
+
     while (1)
     {
         line = display_prompt();
         command = split_line(line);
-        /*for ( i = 0; command[i]; i++)
-        {
-            printf("%s\n", command[i]);
-            free(command[i]), command[i]=NULL;
-        }
-        free(command);*/
+
         stat = execute_command(command, argv, env);
-       
+        for ( i = 0; command[i]; i++)
+        {
+            free(command[i]);
+            command[i] = NULL;
+        }
+        free(command);
     }
-     return (stat);
+    return (stat);
 }
