3349a1127e64f7074291d23d7277bb043dc01a8f
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-12-21 12:29:19 +0100

    update

--- shell.h
+++ shell.h
@@ -8,11 +8,18 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #define DELIM " \t\n"
+extern char **environ;
+
+void freearray(char **command);
 
 char *display_prompt(void);
 char **split_line(char *line);
-int execute_command(char **command, char **argv, char **env);
+int execute_command(char **command, char **argv, char **environ , int indx);
+
+char *_getenv(char *variable);
+char *handle_path(char *command);
 
 #endif
